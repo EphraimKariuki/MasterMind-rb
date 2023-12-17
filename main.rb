1b14552cae7ea1e367dc50082f1d31d8a32dbe53
EphraimKariuki <ephraimkabuikariuki@gmail.com>
2023-12-17 20:14:11 +0300

    Added make_code, gues_code and check_code methods to Game Class and removed them for Codebreaker and codemaker classes

--- main.rb
+++ main.rb
@@ -2,51 +2,52 @@ class Game
     def initialize(codemaker, codebreaker)
       @codemaker = codemaker
       @codebreaker = codebreaker
+      @code = @codemaker.code
+      @guess = @codebreaker.guess
     end
     def play
-      @codemaker.make_code
-      @codebreaker.guess_code
-      @codebreaker.check_code
-    end
-end 
-class Codemaker
-    def initialize
-        @code = []
+        make_code
+        guess_code
+        check_code
     end
     def make_code
         4.times do
-        @code.push(rand(1..6))
+            @code.push(rand(1..4))
         end
         p  @code
-    end
-    # def guess_code
-    #   puts "Guess the code!"
-    #   @codebreaker.guess_code
-    # end
-    # def check_code
-    #   @codebreaker.check_code
-    # end
-end
-class Codebreaker
-    def initialize
-        @guess = []
+
     end
     def guess_code
         puts "Guess the code!"
+        puts "#{@code}"
         @guess = gets.chomp.split("")
+        puts "#{@guess}"
     end
     def check_code
+
         @guess.each_with_index do |num, i|
-            if @code[i] == num
-                puts "You got a match!"
-                puts "You got a match of #{num} at index #{i}"
-            elsif @code.include?(num) 
-                puts "You got a match but in the wrong position!"
-                puts  "You get colour White" 
-            else
-                puts "No match!"
-            end
-        end    
+          if @code[i].to_s == num
+            puts "You got a match!"
+            puts "You got a match of #{num}"
+          elsif @code.include?(num) 
+            puts "You got a match but in the wrong position!"
+            puts  "You get colour White" 
+          else
+            puts "No match!"
+          end
+        end
+    end
+end 
+class Codemaker
+    attr_accessor :code
+    def initialize
+        @code = []
+    end
+end
+class Codebreaker
+    attr_accessor :guess
+    def initialize
+        @guess = []
     end
 end
 game = Game.new(Codemaker.new, Codebreaker.new)
